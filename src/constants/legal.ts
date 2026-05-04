79451816f12c24907b4fc7e882a8b3e94424f466
DJ <poppythemen@gmail.com>
2026-05-04 10:52:51 +0800

    updated ntp numbers

--- src/constants/legal.ts
+++ src/constants/legal.ts
@@ -11,7 +11,7 @@
  * NSW Trade Promotion Notification Number
  * Required for major giveaways in NSW
  */
-export const NTP_NUMBER = "NTP/16264";
+export const NTP_NUMBER = "NTP/16579";
 
 /**
  * NSW Trade Promotion License Number
